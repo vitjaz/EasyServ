e16f267162558dcfdc426f1d43df1b2efeca1f86
edstry <edsamig289@gmail.com>
2019-03-15 11:57:16 +0300

    addVar

--- app/src/main/java/easyserv/aapp/customserv/com/myapplication/LoginActivity.java
+++ app/src/main/java/easyserv/aapp/customserv/com/myapplication/LoginActivity.java
@@ -29,6 +29,8 @@ public class LoginActivity extends AppCompatActivity {
     private FancyButton login;
     private TextView txt_signup;
 
+    private int a = 2;
+
     private FirebaseAuth auth;
     private DatabaseReference databaseReference;
 
