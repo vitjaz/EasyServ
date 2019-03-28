dab5d163d6978e49f1cac7f6c7180a12e217c0b5
edstry <edsamig289@gmail.com>
2019-03-28 20:45:40 +0300

    qwd

---
+++ app/src/main/java/easyserv/aapp/customserv/com/myapplication/Model/Hookah.java
@@ -0,0 +1,134 @@
+package easyserv.aapp.customserv.com.myapplication.Model;
+
+public class Hookah {
+
+    String description;
+    String description_2;
+    String map;
+
+    String image;
+    String photo_1;
+    String photo_2;
+    String photo_3;
+    String photo_4;
+    String photo_5;
+
+    String tel;
+    String time;
+    String title;
+
+    public Hookah(String description, String description_2, String map, String photo_1, String photo_2, String photo_3, String photo_4, String photo_5, String tel, String time, String title, String image) {
+        this.description = description;
+        this.description_2 = description_2;
+        this.map = map;
+        this.photo_1 = photo_1;
+        this.photo_2 = photo_2;
+        this.photo_3 = photo_3;
+        this.photo_4 = photo_4;
+        this.photo_5 = photo_5;
+        this.tel = tel;
+        this.time = time;
+        this.title = title;
+        this.image = image;
+    }
+
+    public Hookah() {
+
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription_2() {
+        return description_2;
+    }
+
+    public void setDescription_2(String description_2) {
+        this.description_2 = description_2;
+    }
+
+    public String getMap() {
+        return map;
+    }
+
+    public void setMap(String map) {
+        this.map = map;
+    }
+
+    public String getPhoto_1() {
+        return photo_1;
+    }
+
+    public void setPhoto_1(String photo_1) {
+        this.photo_1 = photo_1;
+    }
+
+    public String getPhoto_2() {
+        return photo_2;
+    }
+
+    public void setPhoto_2(String photo_2) {
+        this.photo_2 = photo_2;
+    }
+
+    public String getPhoto_3() {
+        return photo_3;
+    }
+
+    public void setPhoto_3(String photo_3) {
+        this.photo_3 = photo_3;
+    }
+
+    public String getPhoto_4() {
+        return photo_4;
+    }
+
+    public void setPhoto_4(String photo_4) {
+        this.photo_4 = photo_4;
+    }
+
+    public String getPhoto_5() {
+        return photo_5;
+    }
+
+    public void setPhoto_5(String photo_5) {
+        this.photo_5 = photo_5;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
---
+++ app/src/main/java/easyserv/aapp/customserv/com/myapplication/ReviewsActivity.java
@@ -0,0 +1,130 @@
+package easyserv.aapp.customserv.com.myapplication;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import easyserv.aapp.customserv.com.myapplication.Model.ReviewObj;
+import easyserv.aapp.customserv.com.myapplication.Model.ReviewsAdapter;
+import easyserv.aapp.customserv.com.myapplication.Model.User;
+
+public class ReviewsActivity extends AppCompatActivity {
+
+    DatabaseReference ref;
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    String userName;
+
+    RecyclerView recyclerView;
+    EditText text;
+    Button button;
+
+    ArrayList<ReviewObj> list;
+
+    ReviewsAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reviews);
+
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        text = findViewById(R.id.reviewsText);
+        button = findViewById(R.id.revBut);
+
+        list = new ArrayList<>();
+        final DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users").child(user.getUid());
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+                userName = user.getUsername();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addReviews();
+                showReviews();
+            }
+        });
+
+        showReviews();
+    }
+
+    private void showReviews() {
+        ref = FirebaseDatabase.getInstance().getReference("Hookah").child("Hookah1").child("Reviews");
+        ref.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                list.clear();
+
+
+
+                ReviewObj reviewObj = dataSnapshot.getValue(ReviewObj.class);
+                Toast.makeText(ReviewsActivity.this, "Sender: " + reviewObj.getSender()
+                        +"\n Text: " + reviewObj.getText(), Toast.LENGTH_SHORT).show();
+                list.add(reviewObj);
+
+                adapter = new ReviewsAdapter(ReviewsActivity.this, list);
+                recyclerView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void addReviews() {
+        ref = FirebaseDatabase.getInstance().getReference("Hookah").child("Hookah1").child("Reviews");
+        HashMap<String, String> map = new HashMap<>();
+        map.put("sender", userName);
+        map.put("text", text.getText().toString());
+        ref.push().setValue(map);
+        adapter.notifyDataSetChanged();
+    }
+}
